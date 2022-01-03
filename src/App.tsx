87854e0301048856306e9171e5a82be3d250985d
paulobeckman <beckmam.paulo@hotmail.com>
2022-01-03 20:39:34 -0300

    feat: callback

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
@@ -25,9 +25,11 @@ export function App() {
     })
   }, [selectedGenreId]);
 
-  function handleClickButton(id: number) {
-    setSelectedGenreId(id);
-  }
+  // function handleClickButton(id: number) {
+  //   setSelectedGenreId(id);
+  // }
+
+  const handleClickButton = useCallback((id: number) => setSelectedGenreId(id), [])
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
